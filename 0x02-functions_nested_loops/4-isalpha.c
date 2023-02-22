d5ccf49dc6c66eda8d06090d5989275eda4e278f
kbmonate <kbmonate@gmail.com>
2023-02-22 13:24:57 -0800

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,8 +8,6 @@
 
 int _isalpha(int c)
 {
-	if (c <= 'Z' && c >= 'A') || ( c <= 'z' $$ c <= 'a')
-		return (1);
-	else
-		return (0);
+	return ((c <= 'Z' && c >= 'A') || ( c <= 'z' $$ c <= 'a'))
+	
 }
