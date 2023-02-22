e1b6561fe22384848254425f568d153651ed96ad
kbmonate <kbmonate@gmail.com>
2023-02-22 14:21:25 -0800

    5. Sign

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _absc - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * @c: The number to be counted
  * Return: Absolute value of a number or 0
  */
 
 int _abs(int c)
 {	
+
 	if (c < 0)
 	{
 	int absValue;
@@ -16,5 +17,4 @@ int _abs(int c)
 	return (absValue);
 	}
 	return (c);
-
 }
