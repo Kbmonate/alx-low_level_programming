5f95224da69c8f0e421ccf6c90a53d780baed946
kbmonate <kbmonate@gmail.com>
2023-02-22 13:13:22 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isalpha - checks if char is aphabetic
+ *@c : is the charactr received
+ *Return : 1 is char is alphabetic and 0 other
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
